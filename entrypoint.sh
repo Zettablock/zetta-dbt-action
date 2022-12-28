457f93770fdcc5034ba7f3fbdc455d7ae708d4cd
Ruimin Shi <srm1120@outlook.com>
2022-12-28 00:39:11 -0800

    update entrypoint.sh use full-refresh for trino tables

--- entrypoint.sh
+++ entrypoint.sh
@@ -67,9 +67,9 @@ elif [[ $trino_result ]]; then
     tasks=( $(git diff --name-status origin/main origin/${PR_BRANCH} |grep -v zettablock_data_mart|grep -v macros|grep trino|grep 'sql$'|grep -v '^D'|cut  -f2 |cut -d'/' -f2-) )
     echo '========================================================='
     echo $tasks
-    echo "dbt build --target dev --profiles-dir ./trino_profile --project-dir ./zettablock --select ${tasks[@]} --vars '{\"external_s3_location\":\"s3://my-897033522173-us-east-1-spark/demo/$(openssl rand -hex 8)/\", \"TRINO_USER\":\"'\"$TRINO_USER\"'\", \"TRINO_PASSWORD\":\"'\"$TRINO_PASSWORD\"'\", \"TRINO_HOST\":\"'\"$TRINO_HOST\"'\"}'"
+    echo "dbt build --target dev --profiles-dir ./trino_profile --project-dir ./zettablock --select ${tasks[@]} --vars '{\"external_s3_location\":\"s3://my-897033522173-us-east-1-spark/demo/$(openssl rand -hex 8)/\", \"TRINO_USER\":\"'\"$TRINO_USER\"'\", \"TRINO_PASSWORD\":\"'\"$TRINO_PASSWORD\"'\", \"TRINO_HOST\":\"'\"$TRINO_HOST\"'\"}' --full-refresh"
     echo '========================================================='
-    echo "dbt build --target dev --profiles-dir ./trino_profile --project-dir ./zettablock --select ${tasks[@]} --vars '{\"external_s3_location\":\"s3://my-897033522173-us-east-1-spark/demo/$(openssl rand -hex 8)/\", \"TRINO_USER\":\"'\"$TRINO_USER\"'\", \"TRINO_PASSWORD\":\"'\"$TRINO_PASSWORD\"'\", \"TRINO_HOST\":\"'\"$TRINO_HOST\"'\"}'"| bash
+    echo "dbt build --target dev --profiles-dir ./trino_profile --project-dir ./zettablock --select ${tasks[@]} --vars '{\"external_s3_location\":\"s3://my-897033522173-us-east-1-spark/demo/$(openssl rand -hex 8)/\", \"TRINO_USER\":\"'\"$TRINO_USER\"'\", \"TRINO_PASSWORD\":\"'\"$TRINO_PASSWORD\"'\", \"TRINO_HOST\":\"'\"$TRINO_HOST\"'\"}' --full-refresh"| bash
     if [ $? -ne 0 ]
         then
             echo "exception."
